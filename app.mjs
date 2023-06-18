4b9cc406f8f5282576e756967f0573f02f4a702d
iori <havemayhopes@gmail.com>
2023-06-19 04:19:54 +0900

    fixed

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 // const express = require("express");
+import path from "path";
 import express from "express";
 import taskRouter from "./router/tasks.mjs";
 import { connectDB } from "./db/connect.mjs";
@@ -7,8 +8,18 @@ env.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
+
+import cors from "cors";
+app.use(cors({
+  origin: "*"
+}));
+
 app.use("/api/v1/tasks", taskRouter);
-app.use(express.static("./public"));
+app.get("*", function (req, res) {
+  const indexHtml = path.resolve("public", "index.html");
+  res.sendFile(indexHtml);
+})
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
